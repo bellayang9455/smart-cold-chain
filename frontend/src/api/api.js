7d0e2b273cae1bb1303992f60171975cafa259f5
yyy <mbellayang@gmail.com>
2026-05-11 00:42:37 +0800

    ?

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "https://smart-cold-api.onrender.com"
+  baseURL: 'https://smart-cold-api.onrender.com'
+
 })
 
 export default api
